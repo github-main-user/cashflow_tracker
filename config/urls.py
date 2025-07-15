9b9763d3c69f7be9fc4933104ec21c6c967313e6
github-main-user <kolya.skripn@mail.ru>
2025-07-15 17:54:52 +0300

    initial commit

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    # admin
+    path("admin/", admin.site.urls),
+    # docs
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
+    path("api/v1/redoc/", SpectacularRedocView.as_view(), name="redoc"),
+]

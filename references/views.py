2c1bd84a77f07d2f28c34b24c1d3640b7107ff50
github-main-user <kolya.skripn@mail.ru>
2025-07-16 11:56:52 +0300

    feat: add filtering and searching to references endpoints

--- references/views.py
+++ references/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from .models import Category, FlowType, Status, SubCategory
 from .serializers import StatusSerializer
@@ -8,19 +9,31 @@ class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
+    filter_backends = (filters.SearchFilter,)
+    search_field = ("name",)
+
 
 class FlowTypeViewSet(viewsets.ModelViewSet):
     queryset = FlowType.objects.all()
     serializer_class = StatusSerializer
 
+    filter_backends = (filters.SearchFilter,)
+    search_field = ("name",)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = StatusSerializer
+
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("flow_type",)
+    search_field = ("name",)
 
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
     queryset = SubCategory.objects.all()
     serializer_class = StatusSerializer
+
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("category",)
+    search_field = ("name",)

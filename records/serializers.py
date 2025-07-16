7c8a61bd94acee999c5c681151671193a34fd9cb
github-main-user <kolya.skripn@mail.ru>
2025-07-16 14:51:12 +0300

    fix: validate consistency of category, subcategory and flowtype in serializer

--- records/serializers.py
+++ records/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from .models import CashFlowRecord
@@ -18,3 +19,33 @@ class CashFlowRecordSerializer(serializers.ModelSerializer):
             "amount",
             "comment",
         )
+
+    def validate_date(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError("Date must be in the future.")
+        return value
+
+    def validate(self, attrs):
+        instance = self.instance
+
+        # Retreive objects from instance when update, on create use provided data
+        if instance:
+            flow_type = attrs.get("flow_type", instance.flow_type)
+            category = attrs.get("category", instance.category)
+            subcategory = attrs.get("subcategory", instance.subcategory)
+        else:
+            flow_type = attrs.get("flow_type")
+            category = attrs.get("category")
+            subcategory = attrs.get("subcategory")
+
+        if category and flow_type and category.flow_type != flow_type:
+            raise serializers.ValidationError(
+                {"category": "Category doesn't belong to given flow_type"}
+            )
+
+        if subcategory and category and subcategory.category != category:
+            raise serializers.ValidationError(
+                {"subcategory": "Subcategory doesn't belong to given category"}
+            )
+
+        return attrs
